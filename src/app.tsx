398791e3c8419e32720efc6c3085fd658d10e8f6
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-02-16 00:08:01 +0100

    feat: 💄 dropdown menu example

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,11 @@
 import { MetaProvider, Title } from "@solidjs/meta";
-import { Router } from "@solidjs/router";
+import { A, Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import { Button } from "@kobalte/core/button";
+import { DropdownMenu } from "@kobalte/core/dropdown-menu";
+import { DropdownMenuRoot } from "@kobalte/core/src/dropdown-menu/dropdown-menu-root.jsx";
 
 export default function App() {
 	return (
@@ -10,8 +13,31 @@ export default function App() {
 			root={(props) => (
 				<MetaProvider>
 					<Title>SolidStart - Basic</Title>
-					<a href="/">Index</a>
-					<a href="/about">About</a>
+					<header class="navbar bg-neutral shadow-sm">
+						<div class="navbar-start">
+							<DropdownMenu>
+								<DropdownMenu.Trigger>
+									<Button class="btn">Menu</Button>
+								</DropdownMenu.Trigger>
+								<DropdownMenu.Portal>
+									<DropdownMenu.Content class="menu dropdown-content z-1 w-52 rounded-box bg-base-100 p-2 shadow-sm">
+										<DropdownMenu.Item>
+											<Button class="btn btn-link" as={A} href="/">
+												Index
+											</Button>
+										</DropdownMenu.Item>
+										<DropdownMenu.Item>
+											<Button class="btn btn-link" as={A} href="/about">
+												About
+											</Button>
+										</DropdownMenu.Item>
+									</DropdownMenu.Content>
+								</DropdownMenu.Portal>
+							</DropdownMenu>
+						</div>
+						<div class="navbar-center">center</div>
+						<div class="navbar-end">end</div>
+					</header>
 					<Suspense>{props.children}</Suspense>
 				</MetaProvider>
 			)}
