2a5495b9b0edd8f0dd19d32f833e36b8a19d0276
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-02-16 01:51:21 +0100

    fix: ♿️ accesability

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,9 @@
+import { DropdownMenu } from "@kobalte/core/dropdown-menu";
 import { MetaProvider, Title } from "@solidjs/meta";
 import { A, Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
-import { Button } from "@kobalte/core/button";
-import { DropdownMenu } from "@kobalte/core/dropdown-menu";
 
 export default function App() {
 	return (
@@ -16,20 +15,26 @@ export default function App() {
 						<div class="navbar-start">
 							<DropdownMenu>
 								<DropdownMenu.Trigger class="btn">Menu</DropdownMenu.Trigger>
-								<DropdownMenu.Portal>
-									<DropdownMenu.Content class="z-1 w-52 rounded-box bg-base-100 p-2 shadow-sm">
-										<DropdownMenu.Item>
-											<Button class="btn btn-link" as={A} href="/">
-												Index
-											</Button>
-										</DropdownMenu.Item>
-										<DropdownMenu.Item>
-											<Button class="btn btn-link" as={A} href="/about">
+								<div class="dropdown">
+									<DropdownMenu.Portal>
+										<DropdownMenu.Content class="menu dropdown-content z-1 w-52 rounded-box bg-base-100 p-2 shadow-sm">
+											<DropdownMenu.Item
+												class="btn btn-link"
+												as={A}
+												href="/index"
+											>
+												Home
+											</DropdownMenu.Item>
+											<DropdownMenu.Item
+												class="btn btn-link"
+												as={A}
+												href="/about"
+											>
 												About
-											</Button>
-										</DropdownMenu.Item>
-									</DropdownMenu.Content>
-								</DropdownMenu.Portal>
+											</DropdownMenu.Item>
+										</DropdownMenu.Content>
+									</DropdownMenu.Portal>
+								</div>
 							</DropdownMenu>
 						</div>
 						<div class="navbar-center">center</div>
